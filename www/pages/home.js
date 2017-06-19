3bc5d80dd820346d6f64e9146c783fff0d976550
Benjamin Zulauf <benjaminzulauf@Benjamins-MacBook-Pro.local>
2017-06-19 14:48:02 -0700

    new home page

--- www/pages/home.js
+++ www/pages/home.js
@@ -1,80 +1,78 @@
 function loadHome() {
     const template = `
-        <div class="splash">
-            <div class="splash-content">
+        <div class="home-hero">
+            <div class="home-hero-content">
                 <h2>
                     Create your dream garden.
                 </h2>
-                <h3>Sign up for a free consultation with one of our top designers.</h3>
-                <form id="signup-form">
-                    <input type="email" name="email" class="inpt-stretch" placeholder="Your email" required autofocus/>
-                    <input type="password" class="inpt-stretch" name="password" required placeholder="Password"/>
-                    <button type="submit" id="start-button" class="btn btn-lg submit-btn">Get Started</button>
+                <div>Sign up for a free consultation with a Garden Spruce designer.</div>
+                <form id="home-form">
+                    <input type="email" name="email" class="inpt-long" placeholder="Your email" required autofocus/>
+                    <button type="submit" id="start-button" class="button-std">Get Started</button>
                 </form>
             </div>
         </div>
-        <div class="section">
-            <h2 class="section-header">About</h2>
-            <div class="row">
-                <div class="col col7">
-                    <p>Whether you want a few perfect pots or a breezy backyard oasis, Garden Spruce makes it easy to create your dream garden.</p>
-                    <p>We'll match you with a handpicked, top-quality designer to help you with every part of the design process, from sketches to blueprints, installation to maintenance.</p>
-                    <a href="#/about">Learn More</a>
+        <div class="section col12">
+            <div class="row section-header">
+                <div class="col col5">
+                    <img src="img/garden3.jpg" class="col5" />
                 </div>
                 <div class="col col1"></div>
+                <div class="col col6">
+                    <h3>Designed for you.</h3>
+                    <p>We make it simple for you to hire an amazing designer. We have handpicked the best up-and-coming garden designers in Seattle to help you create your garden oasis.</p>
+                    <p>No matter how big or small your project, our team can handle it. We will guide you through each step from blueprints, to installation.</p>
+                </div>
+            </div>
+            <h2 class="section-header">How Garden Spruce works</h2>
+            <div class="margin-bottom flex">
                 <div class="col col4">
-                    <img src="img/garden1.jpg" class="home-img" />
+                    <div class="no">1</div>
+                    <h3 class="inline">Book a consultation</h3>
+                    <p>Based on the project details you provide, your dedicated customer service rep will hand select one of our top designers to meet with you for a complimentary consult.</p>
+                </div>
+                <div class="col col4">
+                    <div class="no">2</div>
+                    <h3 class="inline">Buy design time</h3>
+                    <p>After reviewing your custom proposal from your designer, you can either buy a day design package or book time at $130/hr to execute your project, in-person or by-video. See our design package options.</p>
+                </div>
+                <div class="col col4">
+                    <div class="no">3</div>
+                    <h3>Design and Execute</h3>
+                    <p>Depending on your scope of work, your designer will provide design plans, pick plants and hardscape, source materials, and manage contractors and installation. Your dedicated service rep will offer answers and solutions along the way.</p>
                 </div>
             </div>
             <div class="row margin-top">
-                <div class="col col4"><img src="img/garden2.jpg" class="home-img" /></div>
-                <div class="col col4"><img src="img/garden3.jpg" class="home-img" /></div>
-                <div class="col col4"><img src="img/garden4.jpg" class="home-img" /></div>
+                <div class="col col4"><img src="img/garden2.jpg" class="col4" /></div>
+                <div class="col col4"><img src="img/garden3.jpg" class="col4" /></div>
+                <div class="col col4"><img src="img/garden4.jpg" class="col4" /></div>
             </div>
-            <h2 class="section-header">How it works</h2>
-            <div class="margin-bottom">
-                <ol class="ordered-list">
-                    <li>
-                        <div>
-                            <h2 class="inline">Book a consultation</h2>
-                            <h3 class="col8">Based on the project details you provide, your dedicated customer service rep will hand select one of our top designers to meet with you for a complimentary consult.</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <h2 class="inline">Buy time with Garden Spruce</h2>
-                            <h3 class="col8">After reviewing your custom proposal from your designer, you can either buy a day design package or book time at $130/hr to execute your project, in-person or by-video. See our design package options.</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <h2>Design and Execute</h2>
-                            <h3 class="col8">Depending on your scope of work, your designer will provide design plans, pick plants and hardscape, source materials, and manage contractors and installation. Your dedicated service rep will offer answers and solutions along the way.</h3>
-                        </div>
-                    </li>
-                </ol>
+            <div class="row centered margin-top">
+                <a href="#/service" class="button-std">Learn More</a>
             </div>
-            <a href="#/service">Learn More</a>
             <h2 class="section-header">Pricing</h2>
-            <div class="row margin-bottom">
+            <div class="row">
                 <div class="col col4 box price-box">
-                    <h2 class="margin-bottom">Consulation</h2>
-                    <h3 class="margin-bottom">Free</h3>
-                    <div class="text-small">Start with a free consultation either at your home or by phone. You will leave with an estimate and design guidance. You can then choose to continue with your designer by purchasing a package or hourly rate plan.</div>
+                    <h3 class="margin-bottom">Consulation</h3>
+                    <h4 class="margin-bottom">Free</h4>
+                    <p>Start with a free consultation either at your home or by phone. You will leave with an estimate and design guidance. You can then choose to continue with your designer by purchasing a package or hourly rate plan.</p>
                 </div>
                 <div class="col col4 box price-box">
-                    <h2 class="margin-bottom">Package</h2>
-                    <h3 class="margin-bottom">$300</h3>
-                    <div class="text-small">A day package is a great way to go if you have a small project. You pay for a single day and receive a design and plant list. You can then choose to DIY or have us help with the installation.</div>
+                    <h3 class="margin-bottom">Package</h3>
+                    <h4 class="margin-bottom">$300</h4>
+                    <p>A day package is a great way to go if you have a small project. You pay for a single day and receive a design and plant list. You can then choose to DIY or have us help with the installation.</p>
                 </div>
                 <div class="col col4 box price-box">
-                    <h2 class="margin-bottom">Hourly</h2>
-                    <h3 class="margin-bottom">$90/hr</h3>
-                    <div class="text-small">If you have a bigger project, we recommend hiring your designer by the hour. You can book and pay your designer right from our site.</div>
+                    <h3 class="margin-bottom">Hourly</h3>
+                    <h4 class="margin-bottom">$90/hr</h4>
+                    <p>If you have a bigger project, we recommend hiring your designer by the hour. You can book and pay your designer right from our site.</p>
                 </div>
+            </div>
+            <div class="row centered margin-top">
+                <a href="#/service" class="button-std">Get Started</a>
             </div>
         </div>`;
 
     pageElem.innerHTML = template;
-    document.getElementById("signup-form").addEventListener("submit", function(evt){createUserFromForm(evt, "customer")}, false);
+    document.getElementById("home-form").addEventListener("submit", function(evt){createUserFromForm(evt, "customer")}, false);
 }
